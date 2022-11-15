e4d312805de56a97296d9548aeed928424e34731
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-11-15 16:26:04 +0300

    change

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ $(function () {
     var text = $('.intro__about');
     text.fadeTo(0, 0);
     function showAbout() {
-        text.fadeTo(4000, 1);
+        text.fadeTo(2000, 1);
     }
 
     $(window).scroll(function () {
-        $('.intro__content img').fadeTo(2000, 0, showAbout());
+        $('.intro__content img').fadeTo(800, 0, showAbout());
     });
 });

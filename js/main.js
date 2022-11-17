4994bd325fce19ab36ea9ade8554de0420fd785d
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2022-11-17 12:34:42 +0300

    add animate

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 $(function () {
-    var text = $('.intro__about');
+    let text = $('.intro__about');
+    let logo = $('.intro__content img');
+    let video = $('#bgvideo');
+
     text.fadeTo(0, 0);
     function showAbout() {
         text.fadeTo(2000, 1);
     }
+    AOS.init();
+    $(window).scroll(function () {});
 
-    $(window).scroll(function () {
-        $('.intro__content img').fadeTo(800, 0, showAbout());
+    video[0].playbackRate = 0.6;
+
+    let previousScroll = 0;
+    $(window).scroll(() => {
+        let currentScroll = $(this).scrollTop();
+        if (currentScroll > previousScroll) {
+            logo.fadeTo(220, 0, showAbout());
+        } else {
+            text.hide();
+            logo.fadeTo(1000, 1);
+        }
+        return;
     });
 });
